6b524c4309fcec7afffc175c487271f590c97c2e
Md. Jannatul Nayem <nayemalimran106@gmail.com>
2025-11-28 00:41:56 +0600

    Build automation

---
+++ filelist.f
@@ -0,0 +1,38 @@
+#------------------------------
+# Design files
+#------------------------------
+src/rtl/sipo_reg.sv
+
+#------------------------------
+# Interfaces
+#------------------------------
+src/sim/interfaces/ctrl_intf.sv
+src/sim/interfaces/data_intf.sv
+
+#------------------------------
+# Objects
+#------------------------------
+src/sim/objects/seq_item.sv
+src/sim/objects/rsp_item.sv
+src/sim/objects/seq.sv
+
+#------------------------------
+# Components
+#------------------------------
+src/sim/components/seqr.sv
+src/sim/components/driver.sv
+src/sim/components/monitor.sv
+src/sim/components/agent.sv
+src/sim/components/scb.sv
+src/sim/components/env.sv
+
+#------------------------------
+# Tests
+#------------------------------
+src/sim/tests/base_test.sv
+src/sim/tests/test_1.sv
+
+#------------------------------
+# Top
+#------------------------------
+src/sim/tb_top.sv
