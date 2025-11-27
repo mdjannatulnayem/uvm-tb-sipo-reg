2512de58523fa415f9eb4e33764b7d7972d27976
Md. Jannatul Nayem <nayemalimran106@gmail.com>
2025-11-27 20:06:24 +0600

    response item

---
+++ src/sim/rsp_item.sv
@@ -0,0 +1,17 @@
+class rsp_item extends seq_item;
+    `uvm_object_utils(rsp_item)
+
+    unsigned int DATA_WIDTH = 32;
+    bit [DATA_WIDTH-1:0] parallel_out;
+
+    function new(string name = "rsp_item");
+        super.new(name);
+
+        if (!uvm_config_db#(int)::get(uvm_root::get(), "top", "data_width", DATA_WIDTH)) begin
+            `uvm_fatal(get_type_name(),
+                "Failed to get 'data_width' from uvm_config_db. Check testbench configuration.")
+        end
+
+    endfunction
+
+endclass : rsp_item
