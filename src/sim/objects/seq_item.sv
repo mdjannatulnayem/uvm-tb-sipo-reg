3b40732627358e5bcc12d91c71fb8652343a3e79
Md. Jannatul Nayem <nayemalimran106@gmail.com>
2025-11-30 11:14:14 +0600

    Indentation fix

--- src/sim/objects/seq_item.sv
+++ src/sim/objects/seq_item.sv
@@ -1,15 +1,15 @@
 class seq_item extends uvm_sequence_item;
 
-`uvm_object_utils_begin(seq_item)
-    `uvm_field_bit(serial_in, UVM_ALL_ON)
-    `uvm_field_bit(we, UVM_ALL_ON)
-`UVM_OBJECT_UTILS_END()
+    rand bit serial_in;
+    rand bit we;
 
-rand bit serial_in;
-rand bit we;
+    `uvm_object_utils_begin(seq_item)
+        `uvm_field_bit(serial_in, UVM_ALL_ON)
+        `uvm_field_bit(we, UVM_ALL_ON)
+    `UVM_OBJECT_UTILS_END()
 
-function new(string name = "seq_item");
-    super.new(name);
-endfunction
+    function new(string name = "seq_item");
+        super.new(name);
+    endfunction
 
 endclass : seq_item
